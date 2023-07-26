cc49764c9e839f6e11122ef6e4abedd4a6210f5f
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-26 15:19:04 +0530

    add trouble keymaps

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -35,20 +35,20 @@ keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Trouble keymaps
 keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-    { silent = true, noremap = true }
+  { silent = true, noremap = true }
 )
 keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-    { silent = true, noremap = true }
+  { silent = true, noremap = true }
 )
 keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-    { silent = true, noremap = true }
+  { silent = true, noremap = true }
 )
 keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-    { silent = true, noremap = true }
+  { silent = true, noremap = true }
 )
 keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-    { silent = true, noremap = true }
+  { silent = true, noremap = true }
 )
 keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-    { silent = true, noremap = true }
+  { silent = true, noremap = true }
 )
