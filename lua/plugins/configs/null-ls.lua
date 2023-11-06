2c0bb9a4560c8a12d30e92360830f3834ef02619
Abhiseke <abhisheke.acharya@gmail.com>
2023-11-06 10:35:21 +0530

    update null-ls setup func

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -1,27 +1,23 @@
 local null_ls = require("null-ls")
-local b = null_ls.builtins
-
-local sources = {
-  b.formatting.prettierd.with {
-    filetypes = {
-      "html",
-      "markdown",
-      "css",
-      "typescript",
-      "javascript",
-      "typescriptreact",
-      "javascriptreact",
-      "json",
-      "yaml",
-      "svelte",
-      "astro",
-      "css",
-    },
-  },
-  b.formatting.stylua,
-}
 
 null_ls.setup({
   debug = true,
-  sources = sources,
+  sources = {
+    null_ls.builtins.formatting.prettierd.with {
+      filetypes = {
+        "html",
+        "markdown",
+        "typescript",
+        "javascript",
+        "typescriptreact",
+        "javascriptreact",
+        "json",
+        "yaml",
+        "svelte",
+        "astro",
+        "css",
+      },
+    },
+    null_ls.builtins.formatting.stylua,
+  },
 })
